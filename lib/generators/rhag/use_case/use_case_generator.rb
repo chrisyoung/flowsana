d03e5b01a608f3285604f3f435886559eb747f2e
Chris Young <chris@tanga.com>
2015-07-13 21:39:39 -0700

    use _spec

--- lib/generators/rhag/use_case/use_case_generator.rb
+++ lib/generators/rhag/use_case/use_case_generator.rb
@@ -11,11 +11,11 @@ module Rhag
       template "database_repository.rb.erb", "app/models/repository/database/#{repository_name}.rb"
       template "active_record.rb.erb", "app/models/repository/adapters/ar/#{repository_name}.rb"
 
-      template "repository_adapter_spec.rb.erb", "spec/models/repository/adapters/#{repository_name}.rb"
-      template "repository_memory_spec.rb.erb", "spec/models/repository/memory/#{repository_name}.rb"
-      template "repository_database_spec.rb.erb", "spec/models/repository/database/#{repository_name}.rb"
-      template "use_case_spec.rb.erb", "spec/models/use_case/#{file_name}.rb"
-      template "model_spec.rb.erb", "spec/models/#{repository_name}.rb"
+      template "repository_adapter_spec.rb.erb", "spec/models/repository/adapters/#{repository_name}_spec.rb"
+      template "repository_memory_spec.rb.erb", "spec/models/repository/memory/#{repository_name}_spec.rb"
+      template "repository_database_spec.rb.erb", "spec/models/repository/database/#{repository_name}_spec.rb"
+      template "use_case_spec.rb.erb", "spec/models/use_case/#{file_name}_spec.rb"
+      template "model_spec.rb.erb", "spec/models/#{repository_name}_spec.rb"
     end
   end
 end
