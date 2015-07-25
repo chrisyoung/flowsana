e3200cdb83c7128e1ae87448b6dd625a465d7b1f
Chris Young <chris@tanga.com>
2015-07-24 19:37:03 -0700

    tighten it up

--- lib/generators/rhag/use_case/use_case_generator.rb
+++ lib/generators/rhag/use_case/use_case_generator.rb
@@ -1,21 +1,27 @@
 module Rhag
   class UseCaseGenerator < Rails::Generators::NamedBase
     source_root File.expand_path('../../templates', __FILE__)
-    argument :repository_name, :type => :string
+    argument :repository_name,   :type => :string
     argument :repository_action, :type => :string
 
-    def copy_use_case_file
-      template "use_case.rb.erb", "app/models/use_case/#{file_name}.rb"
-      template "model.rb.erb", "app/models/#{repository_name}.rb"
-      template "memory_repository.rb.erb", "app/models/repository/memory/#{repository_name}.rb"
-      template "database_repository.rb.erb", "app/models/repository/database/#{repository_name}.rb"
-      template "active_record.rb.erb", "app/models/repository/adapters/ar/#{repository_name}.rb"
+    ROOT_PATH      = "app/models/"
+    TEST_ROOT_PATH = "spec/models/"
 
-      template "repository_adapter_spec.rb.erb", "spec/models/repository/adapters/#{repository_name}_spec.rb"
-      template "repository_memory_spec.rb.erb", "spec/models/repository/memory/#{repository_name}_spec.rb"
-      template "repository_database_spec.rb.erb", "spec/models/repository/database/#{repository_name}_spec.rb"
-      template "use_case_spec.rb.erb", "spec/models/use_case/#{file_name}_spec.rb"
-      template "model_spec.rb.erb", "spec/models/#{repository_name}_spec.rb"
+    PATHS = {
+      model:               "/",
+      memory_repository:   "repository/memory/",
+      database_repository: "repository/database/",
+      active_record:       "repository/adapters/ar/data/",
+      adapter:             "repository/adapters/ar/" }
+
+    def copy_use_case_file
+      template "use_case.rb.erb", "#{ROOT_PATH}/use_case/#{file_name}.rb"
+      template "use_case_spec.rb.erb", "#{TEST_ROOT_PATH}/use_case/#{file_name}_spec.rb"
+
+      PATHS.keys.each do |fname|
+        template "#{fname.to_s}.rb.erb", "#{ROOT_PATH}#{PATHS[fname]}#{repository_name}.rb"
+        template "#{fname.to_s}_spec.rb.erb", "#{TEST_ROOT_PATH}#{PATHS[fname]}#{repository_name.to_s}_spec.rb"
+      end
     end
   end
 end
