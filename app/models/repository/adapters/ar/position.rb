7a8b620ed8d29acad29b6f0971cb58bb9927c5ae
Chris Young <chris@tanga.com>
2015-07-24 18:59:43 -0700

    hook up adapter

--- app/models/repository/adapters/ar/position.rb
+++ app/models/repository/adapters/ar/position.rb
@@ -2,25 +2,22 @@ module Repository::Adapters::AR
   class Position
     def initialize(listener)
       @listener = listener
-      @data = Repository::Adapters::AR::Data::Position
+      @data     = Repository::Adapters::AR::Data::Position
     end
 
     def find(id)
       @listener.position_adapter_find_success(
-        ::Position.new(@data.find(id).attributes)
-      )
+        ::Position.new(@data.find(id).attributes))
     end
 
     def create(attributes)
       @listener.position_adapter_create_success(
-        ::Position.new(@data.create(attributes).attributes)
-      )
+        ::Position.new(@data.create(attributes).attributes))
     end
 
     def all
       @listener.position_adapter_all_success(
-        @data.all.map {|position| ::Position.new(position.attributes)}
-      )
+        @data.all.map {|position| ::Position.new(position.attributes)})
     end
   end
 end
