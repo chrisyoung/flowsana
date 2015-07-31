3c55f8978075185fc9c5483932f36617c94a6033
Chris Young <chris@tanga.com>
2015-07-30 21:50:26 -0700

    trying to figure out how to save positions

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,44 +1,55 @@
 class PositionsController < ApplicationController
-  listen_for(:get_position_success)         { |position| @position = position }
-  listen_for(:create_position_success)      { |position| redirect_to positions_path }
-  listen_for(:get_positions_success)        { |positions| @positions = positions }
-  listen_for(:list_transitions_for_success) { |transitions| @transitions = transitions }
-  listen_for(:update_position_success)      { |position| redirect_to positions_path }
+  before_filter :init_use_cases
+  before_filter :get_new_position, only: :new
+
+  listen_for(:get_position_success)         { |p| @position    = p }
+  listen_for(:get_positions_success)        { |p| @positions   = p }
+  listen_for(:list_transitions_for_success) { |t| @transitions = t }
+  listen_for(:update_position_success)      { |p| redirect_to positions_path }
+  listen_for(:create_position_success)      { |p| redirect_to positions_path }
 
   def index
-    UseCase::GetPositions.new(self, position_repository).all
+    @get_positions.all
   end
 
   def create
-    UseCase::CreatePosition.new(self, position_repository).create(position_params)
+    @create_position.create position_params
   end
 
   def edit
-    UseCase::GetPosition.new(self, position_repository).get(params[:id])
-    UseCase::GetPositions.new(self, position_repository).all
-    UseCase::ListTransitionsFor.new(self, transition_repository).list(@position)
-  end
-
-  def new
-    @position = Position.new
+    @get_position.get params[:id]
+    @get_positions.all
+    @list_transitions_for.list @position
   end
 
   def update
-    UseCase::GetPosition.new(self, position_repository).get(params[:id])
-    UseCase::UpdatePosition.new(self, position_repository).update(@position, position_params)
+    @get_position.get params[:id]
+    @update_position.update @position, position_params
   end
 
-  private
+private
+
+  def get_new_position
+    @position = Position.new
+  end
+
+  def init_use_cases
+    @create_position      = UseCase::CreatePosition.new(self, position_repo)
+    @get_positions        = UseCase::GetPositions.new(self, position_repo)
+    @get_position         = UseCase::GetPosition.new(self, position_repo)
+    @update_position      = UseCase::UpdatePosition.new(self, position_repo)
+    @list_transitions_for = UseCase::ListTransitionsFor.new(self, transition_repo)
+  end
 
   def position_params
-    params.require(:position).permit(:name)
+    params.require(:position).permit(:to_transitions)
   end
 
-  def position_repository
+  def position_repo
     Repository::Database::Position
   end
 
-  def transition_repository
+  def transition_repo
     Repository::Database::Transition.new
   end
 end
