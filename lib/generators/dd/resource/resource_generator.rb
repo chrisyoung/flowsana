6bc7c2f20fad5bc83d8de3ac3d1fd7350d439816
Chris Young <chris@tanga.com>
2015-08-16 13:57:57 -0700

    figure it out

--- lib/generators/dd/resource/resource_generator.rb
+++ lib/generators/dd/resource/resource_generator.rb
@@ -10,7 +10,7 @@ module Dd
       generate "dd:use_case", "update_#{file_name}", 'update id attributes'
       generate "dd:use_case", "delete_#{file_name}", 'delete id'
       generate "dd:model", file_name, fields.join(" ")
-      generate "migration", "create_#{file_name}", fields.join(" ")
+      generate "migration", "create_#{file_name.pluralize}", fields.join(" ")
     end
   end
 end
--- lib/generators/dd/use_case/use_case_generator.rb
+++ lib/generators/dd/use_case/use_case_generator.rb
@@ -1,14 +1,14 @@
 module Dd
   class UseCaseGenerator < Rails::Generators::Base
     source_root File.expand_path('../../templates', __FILE__)
-    argument :repository_action, :type => :string
     argument :file_name, :type => :string
+    argument :repository_action, :type => :string
     argument :parameters, :type => :array, :default => []
 
 
     def copy_use_case_file
-      template "use_case.rb.erb", "app/models/use_case/#{repository_action}.rb"
-      template "use_case_spec.rb.erb", "spec/models/use_case/#{repository_action}_spec.rb"
+      template "use_case.rb.erb", "app/models/use_case/#{file_name}.rb"
+      template "use_case_spec.rb.erb", "spec/models/use_case/#{file_name}_spec.rb"
     end
   end
 end
