3dc402dc992948507090e7d63b409beb9238a9e2
Chris Young <chris@tanga.com>
2015-08-15 22:56:51 -0700

    method names for use_cases

--- lib/generators/rhag/resource/resource_generator.rb
+++ lib/generators/rhag/resource/resource_generator.rb
@@ -3,11 +3,11 @@ module Rhag
     source_root File.expand_path('../../templates', __FILE__)
 
     def make_files
-      generate "rhag:use_case", file_name, 'list'
-      generate "rhag:use_case", file_name, 'create'
-      generate "rhag:use_case", file_name, 'read'
-      generate "rhag:use_case", file_name, 'update'
-      generate "rhag:use_case", file_name, 'delete'
+      generate "rhag:use_case", "list_#{file_name.pluralize}", 'list'
+      generate "rhag:use_case", "create_#{file_name}", 'create'
+      generate "rhag:use_case", "read_#{file_name}", 'read'
+      generate "rhag:use_case", "update_#{file_name}", 'update'
+      generate "rhag:use_case", "delete_#{file_name}", 'delete'
       generate "rhag:model", file_name
 
     end
