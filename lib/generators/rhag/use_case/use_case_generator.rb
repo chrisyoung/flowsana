be801d0e669052acadf74c625adda34d98b7a612
Chris Young <chris@tanga.com>
2015-08-15 21:48:27 -0700

    use_case only creates use_case

--- lib/generators/rhag/use_case/use_case_generator.rb
+++ lib/generators/rhag/use_case/use_case_generator.rb
@@ -1,34 +1,11 @@
 module Rhag
   class UseCaseGenerator < Rails::Generators::NamedBase
     source_root File.expand_path('../../templates', __FILE__)
-    argument :repository_name,   :type => :string
     argument :repository_action, :type => :string
 
-    ROOT_PATH      = "app/models/"
-    TEST_ROOT_PATH = "spec/models/"
-
-    PATHS = {
-      model:               "/",
-      memory_repository:   "repository/memory/",
-      database_repository: "repository/database/",
-      active_record:       "repository/adapters/ar/data/",
-      adapter:             "repository/adapters/ar/" }
-
     def copy_use_case_file
-      puts "### USE CASE"
-
-      template "use_case.rb.erb", "#{ROOT_PATH}/use_case/#{file_name}.rb"
-
-      puts "### DOMAIN MODEL AND REPOSITORIES"
-      PATHS.keys.each do |fname|
-        template "#{fname.to_s}.rb.erb", "#{ROOT_PATH}#{PATHS[fname]}#{repository_name}.rb"
-      end
-
-      puts "### TESTS"
-      template "use_case_spec.rb.erb", "#{TEST_ROOT_PATH}/use_case/#{file_name}_spec.rb"
-      PATHS.keys.each do |fname|
-        template "#{fname.to_s}_spec.rb.erb", "#{TEST_ROOT_PATH}#{PATHS[fname]}#{repository_name.to_s}_spec.rb"
-      end
+      template "use_case.rb.erb", "app/models/use_case/#{file_name}.rb"
+      template "use_case_spec.rb.erb", "spec/models/use_case/#{file_name}_spec.rb"
     end
   end
 end
