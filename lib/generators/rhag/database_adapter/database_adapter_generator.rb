a8c2aa8349f1ab99603569c30c896a17326743a4
Chris Young <chris@tanga.com>
2015-08-15 22:03:49 -0700

    database adapter

---
+++ lib/generators/rhag/database_adapter/database_adapter_generator.rb
@@ -0,0 +1,10 @@
+module Rhag
+  class DatabaseAdapterGenerator < Rails::Generators::NamedBase
+    source_root File.expand_path('../../templates', __FILE__)
+
+    def copy_model_files
+      template "database_adapter.rb.erb", "app/models/repository/adapters/ar/#{file_name}.rb"
+      template "database_adapter_spec.rb.erb", "spec/models/repository/adapters/ar/#{file_name}_spec.rb"
+    end
+  end
+end
