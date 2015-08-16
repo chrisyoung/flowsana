04a55b2070e5f1680d11cbdb07abb0c747594b1c
Chris Young <chris@tanga.com>
2015-08-16 10:30:57 -0700

    use_case takes parameters and now the correct parameters are generated by resource

--- lib/generators/rhag/resource/resource_generator.rb
+++ lib/generators/rhag/resource/resource_generator.rb
@@ -4,10 +4,10 @@ module Rhag
 
     def make_files
       generate "rhag:use_case", "list_#{file_name.pluralize}", 'list'
-      generate "rhag:use_case", "create_#{file_name}", 'create'
-      generate "rhag:use_case", "read_#{file_name}", 'read'
-      generate "rhag:use_case", "update_#{file_name}", 'update'
-      generate "rhag:use_case", "delete_#{file_name}", 'delete'
+      generate "rhag:use_case", "create_#{file_name}", 'create attributes'
+      generate "rhag:use_case", "read_#{file_name}", 'read id'
+      generate "rhag:use_case", "update_#{file_name}", 'update id attributes'
+      generate "rhag:use_case", "delete_#{file_name}", 'delete id'
       generate "rhag:model", file_name
     end
   end
--- lib/generators/rhag/use_case/use_case_generator.rb
+++ lib/generators/rhag/use_case/use_case_generator.rb
@@ -2,6 +2,8 @@ module Rhag
   class UseCaseGenerator < Rails::Generators::NamedBase
     source_root File.expand_path('../../templates', __FILE__)
     argument :repository_action, :type => :string
+    argument :parameters, :type => :array
+
 
     def copy_use_case_file
       template "use_case.rb.erb", "app/models/use_case/#{file_name}.rb"
