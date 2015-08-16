6f861081da98e41840d7885b649aecd6e25830ad
Chris Young <chris@tanga.com>
2015-08-15 21:59:08 -0700

    database repository

---
+++ lib/generators/rhag/database_repository/database_repository_generator.rb
@@ -0,0 +1,10 @@
+module Rhag
+  class DatabaseRepositoryGenerator < Rails::Generators::NamedBase
+    source_root File.expand_path('../../templates', __FILE__)
+
+    def copy_model_files
+      template "database_repository.rb.erb", "app/models/repository/database/#{file_name}.rb"
+      template "database_repository.rb.erb", "spec/models/repository/database/#{file_name}_spec.rb"
+    end
+  end
+end
