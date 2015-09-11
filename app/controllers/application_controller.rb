0b7c22a83532091aedf5630cc124732632cd9952
Chris Young <chris@tanga.com>
2015-09-11 10:31:47 -0700

    remove these - they are supplied by hexarails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def repository
-    Repository::Database
-  end
-
-  def self.listen_for(message, &block)
-    send :define_method, message, &block
-  end
 end
